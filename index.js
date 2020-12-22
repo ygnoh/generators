ddd7feade185302f7bfb6b48b98ce05972670d8e
yonggoo-noh <yonggoo.noh@navercorp.com>
2020-12-22 10:49:28 +0900

    implement element

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ function integer(from = 0, to = Number.MAX_SAFE_INTEGER, step = 1) {
         }
     };
 }
+
+function element(array, gen = integer(0, array.length)) {
+    return function elementGenerator(...args) {
+        const idx = gen(...args);
+
+        if (idx !== undefined) {
+            return array[idx];
+        }
+    }
+}
